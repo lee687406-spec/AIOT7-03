a3e67a6c46c58429d76dcfeac640581a8ce9cb70
ansrms90 <ansrms90@gmail.com>
2026-08-26 09:13:48 +0900

    2차

--- product/src/DAO/ProductDAO.java
+++ product/src/DAO/ProductDAO.java
@@ -7,9 +7,9 @@ import DTO.Product;
 
 public class ProductDAO extends JDBConnection {
 public List<Product> list(){
-    List<Product> productList= new ArrayList<>();                               //상품목록
+    List<Product> productList= new ArrayList<>();                               //목록
     String sql= " SELECT * "
-              + " FROM board ";
+              + " FROM product ";
 
     try{
         stmt= con.createStatement();
@@ -17,11 +17,11 @@ public List<Product> list(){
         while (rs.next()) {
             Product product= new Product();
             product.setNo(rs.getInt("no"));                         //no 상품번호
-            product.setTitle(rs.getString("title"));                //title?
-            product.setWriter(rs.getString("writer"));              //writer?
-            product.setContent(rs.getString("content"));            //content?
-            product.setCreatedAt(rs.getTimestamp("created_at"));    //createdAt 생성날짜
-            product.setUpdatedAt(rs.getTimestamp("updated_at"));    //updatedAt 수정날짜
+            product.setName(rs.getString("name"));                  //title-> name 상품명
+            product.setPrice(rs.getInt("price"));                   //writer-> price 가격, getString-> getInt
+            product.setDescription(rs.getString("description"));    //content-> description 설명
+            product.setCreatedAt(rs.getTimestamp("created_at"));    //createdAt 등록일자
+            product.setUpdatedAt(rs.getTimestamp("updated_at"));    //updatedAt 수정일자
 
             productList.add(product);
         }
@@ -51,7 +51,7 @@ public Product select(int no){
         if (rs.next()) {
             product.setNo(rs.getInt("no"));                         //no 상품번호
             product.setName(rs.getString("name"));                  //title-> name 상품명
-            product.setPrice(rs.getString("price"));                //writer-> price 가격
+            product.setPrice(rs.getInt("price"));                   //writer-> price 가격, getString-> getInt
             product.setDescription(rs.getString("description"));    //content-> description 설명
             product.setCreatedAt(rs.getTimestamp("created_at"));    //createdAt 등록일자
             product.setUpdatedAt(rs.getTimestamp("updated_at"));    //updatedAt 수정일자
@@ -70,13 +70,13 @@ public Product select(int no){
  */
 public int insert(Product product){
     int result= 0;
-    String sql= " INSERT INTO product ( no, title, writer, content ) "          //no, title, writer, content
+    String sql= " INSERT INTO product ( no, name, price, description ) "       //title-> name, writer-> price, content-> description
               + " VALUES ( PRODUCT_SEQ.nextval, ?, ?, ? ) ";
     try{
         psmt= con.prepareStatement(sql);
-        psmt.setString(1, product.getName());                  //getTitle
-        psmt.setString(2, product.getPrice());                 //getWriter
-        psmt.setString(3, product.getDescription());           //getContent
+        psmt.setString(1, product.getName());                  //getTitle -> getName
+        psmt.setInt(2, product.getPrice());                    //getWriter ->getPrice, setstring -> setInt
+        psmt.setString(3, product.getDescription());           //getContent -> getDescription
         result= psmt.executeUpdate();
     } catch(Exception e){
         System.err.println("상품등록시, 예외 발생");
@@ -94,16 +94,16 @@ public int insert(Product product){
 public int update(Product product){
     int result= 0;
     String sql= " UPDATE product "
-              + " SET title = ? "                                               //title
-              + " , writer = ? "                                                //writer
-              + " , content = ? "                                               //content
+              + " SET name = ? "                                               //title -> name
+              + " , price = ? "                                                //writer -> price -> ???
+              + " , description = ? "                                          //content -> description
               + " , updated_at = sysdate "
               + " WHERE no= ? ";
     try{
         psmt= con.prepareStatement(sql);
-        psmt.setString(1, product.getTitle());                  //title
-        psmt.setString(2, product.getWriter());                 //writer
-        psmt.setString(1, product.getContent());                //content
+        psmt.setString(1, product.getName());                  //getTitle -> getName
+        psmt.setInt(2, product.getPrice());                 //getWriter ->getPrice
+        psmt.setString(3, product.getDescription());           //getContent -> getDescription
         psmt.setInt(4, product.getNo());
         result= psmt.executeUpdate();
     } catch (Exception e){
